5f0d456a011dff2604e7af09edca49398a181e44
tuto <hss2@cin.ufpe.br>
2022-04-03 21:33:37 -0300

    Change length of line and casting sizes

--- src/algorithms/kmp.cpp
+++ src/algorithms/kmp.cpp
@@ -1,5 +1,4 @@
 #include "kmp.h"
-
 using namespace std;
 
 KMP::KMP(vector<string> &patterns) {
@@ -25,21 +24,21 @@ void KMP::get_pattern_borders(int pos) {
 
 vector<Occurrence> KMP::get_occurrences(string &text) {
     vector<Occurrence> occurrences;
-
     for (int pat_pos = 0; pat_pos < this->patterns.size(); pat_pos++) {
         int i,j;
         i = j = 0;
         string current_pattern = this->patterns.at(pat_pos);
         vector<int> current_borders = this->patterns_borders.at(pat_pos);
-        while (i<text.size()-current_pattern.size()){
-            while(i<current_pattern.size() && text.at(i+j) == current_pattern.at(j)){
+        while (i<=(int)text.size()-(int)current_pattern.size()){
+            while(j<current_pattern.size() && text.at(i+j) == current_pattern.at(j)){
                 j++;
             }
-            if(j == current_pattern.size()) occurrences.emplace_back(i,pat_pos);
-            i+= j-current_borders.at(j);
+            if(j == current_pattern.size()) {
+                occurrences.emplace_back(i+current_pattern.size()-1,pat_pos);
+            }
+            i= i+(j-current_borders.at(j)) ;
             j = current_borders.at(j) > 0 ? current_borders.at(j) : 0;
         }   
     }
-
     return occurrences;
 }
--- src/utils.cpp
+++ src/utils.cpp
@@ -7,7 +7,7 @@ Occurrence::Occurrence(int text_position, int pattern_index):
     text_position(text_position), pattern_index(pattern_index) {}
 
 vector<string> Utils::read_lines(string filename) {
-    const int MAX = 1e5 + 5;
+    const int MAX = 1e6 + 5;
     FILE *file = fopen(filename.c_str(), "r");
 
     char buffer[MAX];
