125f07eccef8a431754c53fb1caf73e471c83a97
tuto <hss2@cin.ufpe.br>
2022-04-03 21:52:31 -0300

    fixing sellers

--- src/algorithms/sellers.cpp
+++ src/algorithms/sellers.cpp
@@ -16,14 +16,14 @@ vector<Occurrence> Sellers::get_occurrences(string &text) {
         vector<vector<int>> edit_dist = get_edit_dist(text,current_pattern);
         for(int j=1; j<=text.size(); j++){
             for(int i=1; i<=current_pattern.size(); i++){
-                int replace = current_pattern.at(i) == text.at(j) ? 0 : 1;
+                int replace = current_pattern.at(i-1) == text.at(j-1) ? 0 : 1;
                 edit_dist.at(i).at(j) = min(
                     edit_dist.at(i-1).at(j)+1,
                     edit_dist.at(i).at(j-1)+1,
                     edit_dist.at(i-1).at(j-1)+replace
                 );
             }
-            if(edit_dist.at(current_pattern.size()).at(j)<=max_edit_distance){
+            if(edit_dist.at((int)current_pattern.size()).at(j)<=max_edit_distance){
                 occurrences.emplace_back(j,pat_pos);
             }
         }
@@ -36,10 +36,10 @@ vector<vector<int>> Sellers::get_edit_dist(string &text, string &pattern){
     int n = text.size()+1;
     int m = pattern.size()+1;
     vector<vector<int>> edit_dist = vector<vector<int>>(m);
-    edit_dist.at(0).assign(n,0);
+    edit_dist[0].assign(n,0);
     for(int i=1; i<edit_dist.size();i++){
-        edit_dist.at(i).resize(m);
-        edit_dist.at(i).at(0) = i;
+        edit_dist[i].resize(n);
+        edit_dist[i].at(0) = i;
     }
     return edit_dist;
 }
