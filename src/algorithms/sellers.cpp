59443a451cd6c136c81ed5c1a6a1a53a023da849
Leonardo Anjos <leonardoanjos.1a2015@gmail.com>
2022-04-06 17:23:51 -0300

    Optimization on Sellers

--- src/algorithms/sellers.cpp
+++ src/algorithms/sellers.cpp
@@ -8,44 +8,30 @@ Sellers::Sellers(vector<string> &patterns, int max_edit_distance) {
 }
 
 vector<Occurrence> Sellers::get_occurrences(string &text) {
+    int n = text.size();
     vector<Occurrence> occurrences;
-    for (int pat_pos = 0; pat_pos < this->patterns.size(); pat_pos++) {
-        int i,j;
-        i = j = 0;
-        string current_pattern = this->patterns.at(pat_pos);
-        vector<vector<int>> edit_dist = get_edit_dist(text,current_pattern);
-        for(int j=1; j<=text.size(); j++){
-            for(int i=1; i<=current_pattern.size(); i++){
-                int replace = current_pattern.at(i-1) == text.at(j-1) ? 0 : 1;
-                edit_dist.at(i).at(j) = min(
-                    edit_dist.at(i-1).at(j)+1,
-                    edit_dist.at(i).at(j-1)+1,
-                    edit_dist.at(i-1).at(j-1)+replace
-                );
+    vector<vector<int>> edit_distance;
+
+    for (int pat_pos = 0; pat_pos < (int) patterns.size(); pat_pos++) {
+        int m = patterns[pat_pos].size();
+        edit_distance.assign(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 1; i <= m; i++) {
+            edit_distance[0][i] = i;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                int replace = patterns[pat_pos][j - 1] == text[i - 1] ? 0 : 1;
+                edit_distance[i][j] = min(edit_distance[i - 1][j], edit_distance[i][j - 1]) + 1;
+                edit_distance[i][j] = min(edit_distance[i][j], edit_distance[i - 1][j - 1] + replace);
             }
-            if(edit_dist.at((int)current_pattern.size()).at(j)<=max_edit_distance){
-                occurrences.emplace_back(j,pat_pos);
+
+            if (edit_distance[i][m] <= max_edit_distance) {
+                occurrences.emplace_back(i, pat_pos);
             }
         }
     }
 
     return occurrences;
-}
-
-vector<vector<int>> Sellers::get_edit_dist(string &text, string &pattern){
-    int n = text.size()+1;
-    int m = pattern.size()+1;
-    vector<vector<int>> edit_dist = vector<vector<int>>(m);
-    edit_dist[0].assign(n,0);
-    for(int i=1; i<edit_dist.size();i++){
-        edit_dist[i].resize(n);
-        edit_dist[i].at(0) = i;
-    }
-    return edit_dist;
-}
-
-int Sellers::min(int a, int b, int c){
-    if(a<b && a<c) return a;
-    if(b<c && b<a) return b;
-    return c;
 }
--- src/algorithms/sellers.h
+++ src/algorithms/sellers.h
@@ -9,8 +9,4 @@ private:
 public:
     Sellers(std::vector<std::string> &patterns, int max_edit_distance);
     std::vector<Occurrence> get_occurrences(std::string &text);
-
-private:
-    std::vector<std::vector<int>> get_edit_dist(std::string &text, std::string &pattern);
-    int min(int a, int b, int c);
 };
